af926ae3098d7cecd1d1f01bb5ee59f47f38afb0
Mateo <anfrancor@unal.edu.co>
2026-02-25 14:02:43 -0500

    Mejorando la parte visual de la apliacion, se creo una rama para esto para no afectar el codigo principal

--- App.R
+++ App.R
@@ -1,76 +1,18 @@
 library(shiny)
+library(bslib)
 
-source("R/data_download_processing.R")
-source("R/plots.R")
-
-ui <- fluidPage(
+ui <- page_navbar(
+  title = "Calidad Aire Bogotá",
+  theme = bs_theme(
+    base_font = font_google("Inter"),
+    navbar_bg ="#93BCFA"
+  ),
+  nav_panel("¿Como varian los contaminantes con el tiempo?", "dentro?"),
   
-  titlePanel("Análisis Calidad del Aire"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      
-      dateRangeInput("dates",
-                     "Seleccione rango de fechas",
-                     start = Sys.Date()-7,
-                     end   = Sys.Date()),
-      
-      selectInput("station",
-                  "Seleccione estación",
-                  choices = rmcab_aqs$aqs),
-      
-      selectInput("pollutant",
-                  "Seleccione contaminante",
-                  choices = c("pm10","pm2.5")),
-      
-      selectInput("plot_type",
-                  "Seleccione grafica",
-                  choices =c("Variacion contaminante con el tiempo", 
-                             "Rosa de contaminantes"))
-      
-    ),
-    
-    mainPanel(
-      plotOutput("main_plot")
-    )
-  )
 )
 
-server <- function(input, output, session){
-  
-  
-  
-  output$main_plot <- renderPlot({
-    data_reactive <- reactive({
-      
-      req(input$dates, input$station)
-      
-      
-      get_data_clean(
-        aqs = input$station,
-        start_date = format(input$dates[1], "%d-%m-%Y"),
-        end_date   = format(input$dates[2], "%d-%m-%Y")
-      )
-    })
-    
-    req(data_reactive())
-    
-    if(input$plot_type =="Variacion contaminante con el tiempo"){
-      plot_time_variation(
-        data = data_reactive(),
-        pollutant = input$pollutant
-      )
-    } else if (input$plot_type == "Rosa de contaminantes"){
-      
-      plot_pollution_rose(
-        data = data_reactive(),
-        pollutant = input$pollutant
-      )
-    }
-    
-    
-  })
-  
-}
+
+
+server <- function (input, output, session){}
 
 shinyApp(ui, server)
